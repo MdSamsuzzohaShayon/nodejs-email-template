dd732fa9b164ba32531346695a7f2354e2c304c4
Md Shayon <mdshayon0@gmail.com>
2021-03-21 08:17:59 +0600

    drag and drop is working perfectly

--- html/js/main.js
+++ html/js/main.js
@@ -38,6 +38,12 @@ let title = null;
 let rowList = []; // THIS IS FOR TRACKING ROW DETAIL - FOR EXAMPLE - FIRST ROW WITH 3 COLUMN, 2ND ROW WITH ONE COLUMN, 3RD COLUMN WITH 2 COLUMN
 let rowID = 1; // AUTO INCREMENT
 let rowWithColumn;  // WHICH ROW IS SELECTING , 1 COLUMN ROW , 2 COLUMN ROW OR 3 COLUMN ROW
+
+let positionElement = [];   // THIS IS FOR TRACKING WHICH WHICH BLOCK ELEMENT IS HOLDING WHICH POSITION - FOR EXAMPLE IMAGE HOLDER IS TAKING ROW-2 AND COLUMN 2
+
+
+
+let blockElementDetails = [];   // THIS IS FOR DETAILS OF THE BLOCK WHICH WE HAVE MADE 
 // DATABASE DESIGN ENDS 
 
 
@@ -130,7 +136,9 @@ function columnDragAndDrop() {
 
                     // https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
                     const newDiv = document.createElement('div');
-                    newDiv.setAttribute('class', 'drop-row');
+                    // setAttributes(elem, {"src": "http://example.com/something.jpeg", "height": "100%", ...});
+                    setAttributes(newDiv, { "class": "drop-row", "id": `row-${rowID}` });
+                    // newDiv.setAttribute('class', 'drop-row');
 
 
                     // ADD DEFFERENT TYPE OF STYLING FOR DEFFERENT TYPE OF COLUMN ELEMENT 
@@ -138,7 +146,7 @@ function columnDragAndDrop() {
                         case 'col-1-grid':
                             // ADD 1 COLUMN INSIDE A DIV
                             const oneColumnDiv = document.createElement('div');
-                            oneColumnDiv.setAttribute('class', 'one-column-div');
+                            setAttributes(oneColumnDiv, { "class": "one-column-div", "id": 'one-col-0' });
                             newDiv.appendChild(oneColumnDiv);
                             rowWithColumn = 1;
                             break;
@@ -146,7 +154,7 @@ function columnDragAndDrop() {
                             // ADD 2 COLUMN INSIDE A DIV 
                             for (let i = 0; i < 2; i++) {
                                 const twoColumnDiv = document.createElement('div');
-                                twoColumnDiv.setAttribute('class', 'two-column-div');
+                                setAttributes(twoColumnDiv, { "class": "two-column-div", "id": `two-col-${i}` });
                                 newDiv.appendChild(twoColumnDiv);
                                 rowWithColumn = 2;
                             }
@@ -154,10 +162,10 @@ function columnDragAndDrop() {
                         case 'col-3-grid':
                             // ADD 3 COULMN INSIDE A DIV
                             for (let i = 0; i < 3; i++) {
-                                const threeColumnDiv = document.createElement('div');
-                                threeColumnDiv.setAttribute('class', 'three-column-div');
-                                newDiv.appendChild(threeColumnDiv);
                                 rowWithColumn = 3;
+                                const threeColumnDiv = document.createElement('div');
+                                setAttributes(threeColumnDiv, { "class": "three-column-div", "id": `three-col-${i}` });
+                                newDiv.appendChild(threeColumnDiv);
                             }
                             break;
                         default:
@@ -229,6 +237,28 @@ columnDragAndDrop();
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // PLANNING 
 // THIS WAY IS NOT WORKING SO NOW I AM GOING TO APPEND ALL BLOCK ELEMENT INTO DROP ZONE (ELEMENT ID)
 // BY USING CSS I WILL STYLE TOP OF THE COLUMN DIV BY USING POSITION ABSOLUTE 
@@ -240,8 +270,22 @@ columnDragAndDrop();
 // OR WE CAN SEECT DIRECTLY FROM THE ELEMENT // document.addEventListener   == IT'S WORKED 😊
 
 
+
+// BY POSITION FIND OUT THE COLUMN INDEX NUMBER 
+// FIND OUT IN WHICH COLUMN AND WHICH ROW OUR ELEMENT IS LOCATED
+// SET ONE MORE ATTRIBUTE IN COLUMN BLOCK THAT CAN DIFFERENTTHIATE EACH ELEMENT 
+
+
+
+
+// AT FIRST GET ROW ID AND COL ID 
+
+
 function blockDragAndDrop() {
     let dropableBlock = null;
+    let rowNumber;
+    let columnNumber;
+    let blockElement = null;
 
 
     contentBlockCol.forEach((blockCol, index) => {
@@ -298,40 +342,53 @@ function blockDragAndDrop() {
         //     // console.log("Mouseover - Event: ", e.toElement.className);
         // }
         // console.log("dragover - Event: Prevent default");
-        // console.log("Mouseover - Event: ", e.toElement.id);
+        // console.log("Mouseover - Event: ", e.path[1].classList);
+        // console.log(e.target);
+
     });
 
 
 
+    console.log(rowList);
     document.addEventListener('drop', (e) => {
+        // console.log("Mouseover - Event: ", e.toElement.id);   // GETTING COLUMN ID (iNDICATES INDEX NUMBER OF THE COLUMN)
+
+        // console.log("Mouseover - Event: ", e.path[1].id); // // GETTING ROW ID (iNDICATES INDEX NUMBER OF THE ROW)
         // console.log("Targeter element", e.target.className); // Result - three-column-div
         if (e.toElement.id !== 'drop-zone') {
             // console.log(e.toElement.id);
             let dropableColumn = e.target.className;
-            if (dropableColumn === 'one-column-div') {
-                console.log("Drop - One E: ", e);
-            } else if (dropableColumn === 'two-column-div') {
-                console.log("Drop - Two E: ", e);
-            } else if (dropableColumn == 'three-column-div') {
-                console.log("Drop - Three E: ", e);
-            }
+            // if (dropableColumn === 'one-column-div') {
+            //     // console.log("Drop - One E: ", e);
+            // } else if (dropableColumn === 'two-column-div') {
+            //     // console.log("Drop - Two E: ", e);
+            // } else if (dropableColumn == 'three-column-div') {
+            //     // console.log("Drop - Three E: ", e);
+            // }
             // ONLY DROP WHEN DROPABLE ELEMENT IS COLUMN 
             if (dropableColumn === "one-column-div" || dropableColumn === "two-column-div" || dropableColumn === "three-column-div") {
                 // CHECK FOR WHICH COLUMN WE ARE DROPING THE BLOCK ELEMENT 
 
                 if (dropableBlock === "img-holder" || dropableBlock === "txt-holder" || dropableBlock === "btn-holder" || dropableBlock === "social-holder" || dropableBlock === "spx-holder") {
+
                     // console.log("Drop - Dropable Block: ", dropableBlock);
                     // console.log("Drop event : ", e);
-                    // console.log("Drop - Event: ", e.toElement);
+                    console.log("Drop - Event: ", e.target);
                     const newBlockCol = document.createElement('div');
-                    newBlockCol.setAttribute('id', "working");
-                    newBlockCol.textContent = 'Hello world 😊';
+                    newBlockCol.setAttribute('class', "block");
+                    newBlockCol.textContent = dropableBlock + ' 😊';
                     e.toElement.appendChild(newBlockCol);
+
+                    rowNumber = convertRowIdToNumber(e.path[1].id);
+                    columnNumber = convertColIdToNumber(e.toElement.id);
+                    blockElement = dropableBlock;
+                    positionElement.push({ rowNumber, columnNumber, blockElement });
+                    // console.log(positionElement);
                 } else {
                     console.log("Drop - Not Dropable Block: ", dropableBlock);
                 }
             } else {
-
+                console.log("Not dropable column");
             }
         };
     });
@@ -339,6 +396,106 @@ function blockDragAndDrop() {
 
 
 blockDragAndDrop();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// EXTRA FUNCTIONS 
+function setAttributes(el, attrs) {
+    for (let key in attrs) {
+        el.setAttribute(key, attrs[key]);
+    }
+}
+
+
+
+
+
+
+
+
+function convertRowIdToNumber(rowElementId) {
+    switch (rowElementId) {
+        case 'row-1':
+            rowNumber = 1;
+            break;
+        case 'row-2':
+            rowNumber = 2;
+            break;
+        case 'row-3':
+            rowNumber = 3;
+            break;
+        default:
+            break;
+    }
+    return rowNumber;
+}
+function convertColIdToNumber(colElementId) {
+    switch (colElementId) {
+        case 'one-col-0':
+            columnNumber = 1;
+            break;
+        case 'two-col-0':
+            columnNumber = 1;
+            break;
+        case 'two-col-1':
+            columnNumber = 2;
+            break;
+        case 'three-col-0':
+            columnNumber = 1;
+            break;
+        case 'three-col-1':
+            columnNumber = 2;
+            break;
+        case 'three-col-2':
+            columnNumber = 3;
+            break;
+
+        default:
+            break;
+    }
+    return columnNumber;
+}
 
 
 
