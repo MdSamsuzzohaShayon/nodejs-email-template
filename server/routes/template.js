e5ba25bc209333400df31c9def941b9d12afd9aa
Md Shayon <mdshayon0@gmail.com>
2021-04-06 17:21:45 +0600

    front end clean up

--- server/routes/template.js
+++ server/routes/template.js
@@ -19,7 +19,12 @@ router.get('/editor', (req, res, next) => {
 });
 
 
-router.post('/add', (req, res, next) => {
+const arrayOfImg = [{ name: 'img1', maxCount: 1 }, { name: 'img2', maxCount: 1 }];
+
+
+router.post('/add', uploadFile.fields(arrayOfImg), (req, res, next) => {
+    console.log(req.files);
+    console.log(req.body);
     res.status(200).json({ request: 'Success' });
 });
 
