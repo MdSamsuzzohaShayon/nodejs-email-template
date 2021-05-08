d7e6b6888fe6c7f13c34d4a27b6bb357cb3abce5
Md Shayon <mdshayon0@gmail.com>
2021-05-09 02:22:11 +0600

    json response

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('template/index');
+  res.status(200).json({ "msg": "success" });
 });
 
 module.exports = router;
