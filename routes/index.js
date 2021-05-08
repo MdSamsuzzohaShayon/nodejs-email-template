b01c27ca2f58cdb533fe2748695a4530f0a676c5
Md Shayon <mdshayon0@gmail.com>
2021-05-09 02:08:46 +0600

    adding index route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.status(200).json({ "msg": "success" });
+  res.render('template/index');
 });
 
 module.exports = router;
